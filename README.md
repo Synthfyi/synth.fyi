4befed05cf90abd03057fd1c11c9137bb8ddf350
Synth <119522104+Synthfyi@users.noreply.github.com>
2023-05-23 19:58:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Synth.fyi
+ https://synth.fyi
---
+++ main.js
@@ -0,0 +1,27 @@
+// Run functions
+window.onload = function() {
+
+    setAge()
+
+}
+
+// Dynamic Age
+function setAge() {
+
+    birthDate = new Date("14 January 2010");
+    otherDate = new Date();
+
+    element = document.getElementById("dynamicAge");
+
+    if (!element) return;
+
+    var years = (otherDate.getFullYear() - birthDate.getFullYear());
+
+    if (otherDate.getMonth() < birthDate.getMonth() || 
+        otherDate.getMonth() == birthDate.getMonth() && otherDate.getDate() < birthDate.getDate()) {
+        years--;
+    }
+
+    element.innerHTML = years;
+
+}
